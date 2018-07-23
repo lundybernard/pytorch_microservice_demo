fdca6686b1f120a1bb4726189c38429c70654872
LundyBernard <lundy.bernard@gmail.com>
2018-07-23 11:37:00 -0500

    add integration tests microservice web interface

---
+++ integration_tests/pytorch_server_test.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+from time import time
+import requests
+from compose.cli.main import TopLevelCommand, project_from_options
+
+
+class DockerComposeIntegrationTests(TestCase):
+
+    def setUp(self):
+        path = './'  # Path to docker-compose directory
+        self.options = {
+            "--no-deps": False,
+            "--abort-on-container-exit": False,
+            "SERVICE": "",
+            "--remove-orphans": False,
+            "--no-recreate": True,
+            "--force-recreate": False,
+            "--build": False,
+            '--no-build': False,
+            '--no-color': False,
+            "--rmi": "none",
+            "--volumes": "",
+            "--follow": False,
+            "--timestamps": False,
+            "--tail": "all",
+            "-d": True,
+            '--always-recreate-deps': False,
+            '--scale': []
+        }
+
+        project = project_from_options(path, self.options)
+        self.compose = TopLevelCommand(project)
+
+        self.compose.up(self.options)
+        time.sleep(1)  # wait for the server to be ready
+
+    def tearDown(self):
+        self.compose.down(self.options)
+
+    def test_compose_webservice_exists(self):
+        out = requests.get('http://0.0.0.0:5000/')
+        print(self.compose.project.__dict__)
+        self.assertEqual(out.text, 'Hello World!\n')
+
+        time.sleep(1)
+        out = requests.get('http://0.0.0.0:5000/')
+        self.assertEqual(out.text, 'Fail on purpose\n')
