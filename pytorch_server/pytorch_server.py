b93e75434af7c5437441ba8811e6780171c55ad5
LundyBernard <lundy.bernard@gmail.com>
2018-07-27 15:43:12 -0500

    Add a few basic flask route tests

--- pytorch_server/pytorch_server.py
+++ pytorch_server/pytorch_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 from services import cuda_is_available
 
@@ -8,6 +8,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_api():
     return 'Hello World!\n'
+
+
+@app.route('/hello_i_am')
+def hello_i_am():
+    '''/hello_i_am/?name=alice
+    '''
+    name = request.args.get('name', False)
+    if name:
+        return 'Hello {}!\n'.format(name)
+    return 'Hello, whats your name?\n'
+
+
+@app.route('/hello_i_am/<string:name>')
+def hello_i_am_api(name):
+    '''/hello_i_am/alice
+    '''
+    return 'Hello {}!\n'.format(name)
 
 
 @app.route('/cuda_is_available')
