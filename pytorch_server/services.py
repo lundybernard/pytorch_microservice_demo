afb5553fc11e82cbf74ed17fb0de8397ce068621
LundyBernard <lundy.bernard@gmail.com>
2018-07-27 15:38:46 -0500

    Add global flag for cuda availability

--- pytorch_server/services.py
+++ pytorch_server/services.py
@@ -1,6 +1,9 @@
 from torch import cuda
 
 
+CUDA_ENABLED = cuda.is_available()
+
+
 # check GPU availability
 def cuda_is_available() -> str:
     return str(cuda.is_available())
--- pytorch_server/tests/services_test.py
+++ pytorch_server/tests/services_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from pytorch_server.services import cuda_is_available
+from pytorch_server.services import cuda, CUDA_ENABLED, cuda_is_available
 
 
 class ServicesTest(TestCase):
@@ -8,4 +8,14 @@ class ServicesTest(TestCase):
         '''This test fails durring the docker build process
         but it succeeds after the container is built.
         '''
-        self.assertEqual(cuda_is_available(), 'True')
+        if CUDA_ENABLED:
+            self.assertEqual(cuda_is_available(), 'True')
+        else:
+            self.assertEqual(cuda_is_available(), 'False')
+
+
+    def test_global_cuda_flag(self):
+        if cuda.is_available():
+            self.assertTrue(CUDA_ENABLED)
+        else:
+            self.assertFalse(CUDA_ENABLED)
